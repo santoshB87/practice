98e6370b0e415a9d7ead85fb3d618c3e9e825f9f
Santosh Bisht <santbi@dunnhumby.com>
2021-04-12 12:49:16 +0530

    added longest sum in subarray

---
+++ algo/0000.py
@@ -0,0 +1,19 @@
+l = [1, 2, 0, 0, 3, 0, 6]
+ln = len(l)
+i=0
+j=0
+while i < ln:
+    if l[i] == 0:
+        if j == 0:
+            j = i
+        else:
+            j = j+1
+        while i < ln:
+            if l[i] != 0:
+                l[j] = l[i]
+                l[i] = 0
+                break
+            i = i+1
+    i = i+1
+print l
+
